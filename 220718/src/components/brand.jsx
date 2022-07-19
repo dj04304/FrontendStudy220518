b908c66af19b65274c70cb95392ceecb8f95a246
junhyeong <dj403045@gmail.com>
2022-07-19 12:36:05 +0900

    220719 react 클론코딩

--- 220718/src/components/brand.jsx
+++ 220718/src/components/brand.jsx
@@ -1,15 +1,24 @@
 import React from "react";
+import styles from "./brand.module.css";
+
+import BrandItem from "./brandItem";
 import Footer from "./footer";
 
 function Brand() {
     return(
-        <> 
-            <div>
-                Brand
-            </div>
-            
+        <div> 
+            <div className={styles.mainImg}></div> 
+                <div className={styles.contents}>
+                    <div className={styles.contents_in}>
+                        <div className={styles.ttl}>
+                            <h3>BEST BRAND</h3>
+                        </div>
+                        <BrandItem/>
+                    </div>    
+                </div>                
+
             <Footer/>
-        </>
+        </div>
     );
 }
 
--- 220718/src/components/coupon.jsx
+++ 220718/src/components/coupon.jsx
@@ -1,15 +1,67 @@
 import React from "react";
 import Footer from "./footer";
+import styled from "styled-components";
+
+const MainImg = styled.div`  
+   width: 100%;
+   height: 200px;
+
+   background-color: red;
+`
+
+const Contents = styled.div`
+    width: 100%;
+    height: 2000px;
+    background-attachment: #ccc;
+`
+
+const ContentIn = styled.div`
+    display: flex;
+    justify-content: space-around;
+
+    margin: 0px auto;
+
+    width: 1500px;
+    height: 2000px;
+    background-color: aqua;
+`
+
+const FlexBox = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+
+    padding-top: 100px;
+
+    height: 500px;
+    
+`
+
+const Box = styled.div`
+    width: 600px;
+    height: 200px;
+
+    background-color: brown;
+`
 
 function Coupon() {
     return(
-        <> 
-            <div>
-                Coupon
-            </div>
-            
+        <div> 
+            <MainImg/>
+            <Contents>
+                <ContentIn>
+                    <FlexBox>
+                        <Box/>
+                        <Box/>
+                    </FlexBox>
+                    <FlexBox>
+                        <Box/>
+                        <Box/>
+                    </FlexBox>
+                </ContentIn>
+            </Contents>
             <Footer/>
-        </>
+        </div>
     );
 }
 
